af2ef6abbdbcf778ffb59ce272650420706873c7
AbhayRana14 <14abhayrana@gmail.com>
2021-02-08 14:54:44 +0530

    Carousal-Scroll-title added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import MyCarousal from './components/Carousal/carousal.component';
 
 //components
 import MyNavbar from "./components/Navbar/navbar.component";
+import TitleMessage from './components/Title Message/titleMessage.component';
 
 function App() {
   return (
     <div className="App">
       <MyNavbar/>
-      OK
+      <MyCarousal/>
+      <TitleMessage/>
     </div>
   );
 }
